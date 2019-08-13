37c078a4ccdf8180127819143a2411408a5af013
Gilles Coolen <gilleswhc@gmail.com>
2019-08-13 23:37:51 +0200

    Add pages

---
+++ pages/contact.vue
@@ -0,0 +1,69 @@
+<template>
+    <b-container style="margin-top: 10rem; margin-bottom: 10rem;">
+        <b-row>
+            <b-col cols="12">
+                <h1 style="color: #8c5c11;">Contact</h1>
+            </b-col>
+            <b-col>
+                <header>E-mail</header>
+                <span>Heeft u vragen?</span>
+                <br />
+                <span>Stuur ons direct een e-mail!</span>
+                <br />
+                <br />
+                <p class="h6">
+                    <a href="mailto:info@040vastgoedontwikkeling.nl">
+                        <fa class="icon" icon="envelope" />info@040vo.nl
+                    </a>
+                </p>
+                <header>Telefoon</header>
+                <span>Liever bellen?</span>
+                <br />
+                <span>We zijn telefonisch bereikbaar!</span>
+                <br />
+                <br />
+                <p class="h6">
+                    <a href="tel:+31850027569">
+                        <fa class="icon" icon="phone" />085 002 7569
+                    </a>
+                </p>
+            </b-col>
+            <b-col>
+                <header>Adres</header>
+                <span>Stratumsedijk 20</span>
+                <br />
+                <span>5611 ND Eindhoven</span>
+                <br />
+                <br />
+                <span>Postbus 1416</span>
+                <br />
+                <span>5602 BK Eindhoven</span>
+                <br />
+                <br />
+                <p class="h6">
+                    <a href="https://goo.gl/maps/Dm7YhVkpiqvVKkQeA" target="_blank">
+                        <fa class="icon" icon="map-marker-alt" />Open op Google Maps
+                    </a>
+                </p>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+header {
+    color: #8c5c11;
+    font-weight: bold;
+    font-family: "Nunito", sans-serif;
+}
+
+a,
+a:hover {
+    color: #c29c2b;
+}
+</style>
---
+++ pages/werkwijze.vue
@@ -0,0 +1,6 @@
+<template>
+    <b-container style="margin-top: 10rem; margin-bottom: 10rem;">
+        <h1 style="color: #8c5c11;">Werkwijze</h1>
+        <p>040 vastgoedontwikkeling is als projectontwikkelaar actief in het ontwikkelen van projecten door geheel Nederland. De focus van 040 Vastgoedontwikkeling ligt op de ontwikkeling van vastgoed in de zorgsector, wij zijn de verbindende factor tussen zorg en vastgoed. Wij brengen de juiste partijen samen om het optimale resultaat te bereiken voor onze opdrachtgevers en gebruiken hiervoor onze brede kennis en ruime ervaring.</p>
+    </b-container>
+</template>
