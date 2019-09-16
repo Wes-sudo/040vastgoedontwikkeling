1cadda7225326233901de4d2c6669b4a768f36d7
Gilles Coolen <gilleswhc@gmail.com>
2019-09-16 10:05:55 +0200

    Add newspage

---
+++ pages/nieuws/_article.vue
@@ -0,0 +1,73 @@
+<template>
+    <b-container style="margin-top: 7rem; margin-bottom: 5rem;">
+        <b-row>
+            <h1>Vacature parttime allround secretaresse</h1>
+            <p>
+                <span
+                    style="font-weight: 400;"
+                >O4O vastgoedontwikkeling is een snelgroeiende zorgontwikkelaar in hartje Eindhoven.</span>
+            </p>
+            <p>
+                <strong>Wat ga je doen?</strong>
+                <span style="font-weight: 400;">
+                    <br />
+                </span>
+                <span
+                    style="font-weight: 400;"
+                >Wij zoeken een parttime allround secretaresse (v) voor langere periode. In deze uitdagende en veelzijdige functie ben je de rechterhand voor de eigenaar. Je houdt de administratie/boekhouding bij, bent verantwoordelijk voor commerci&euml;le activiteiten en doet alle voorkomende werkzaamheden op kantoor zoals HR en IT. Je bent een echte spin-in-het-web, hebt een pro-actieve houding, bent secuur en kan goed zelfstanding werken. Wij zijn direct en verwachten dit ook van jou.</span>
+            </p>
+            <p>
+                <span
+                    style="font-weight: 400;"
+                >Het betreft een functie voor minimaal 8 uur en maximaal 24 uur per week (in overleg).</span>
+            </p>
+            <p>
+                <strong>Wat neem je mee?</strong>
+                <strong>
+                    <br />
+                </strong>
+                <span
+                    style="font-weight: 400;"
+                >Je beschikt over HBO werk -en denkniveau. Je hebt uitstekende schriftelijke (NL/UK) en mondelinge communicatieve vaardigheden, bent discreet en betrouwbaar en weet prioriteiten te stellen. Je hebt hart voor de zaak en een groot verantwoordelijkheidsgevoel. Wij zoeken iemand die zelfstandig kan werken in een klein, informeel team dat bestaat uit de eigenaar, 1 senior ontwikkelaar, 2 junior ontwikkelaars en een stagiaire.</span>
+            </p>
+            <p>
+                <strong>Wat bieden wij jou?</strong>
+                <strong>
+                    <br />
+                </strong>
+                <span
+                    style="font-weight: 400;"
+                >Wij bieden een leuke, gevarieerde en uitdagende baan in het hartje van Eindhoven, in een energieke werkomgeving. Een marktconform salaris en flexibiliteit in de uren.</span>
+            </p>
+            <p>
+                <strong>Meer informatie?</strong>
+                <strong>
+                    <br />
+                </strong>
+                <span style="font-weight: 400;">
+                    Is dit de uitdagende baan die jij zoekt? Solliciteer dan op deze functie door je CV en motivatie te mailen naar
+                    <a
+                        href="mailto:info@040vastgoedontwikkeling.nl"
+                    >info@040vastgoedontwikkeling.nl</a>.
+                </span>
+            </p>
+            <p>
+                <span style="font-weight: 400;">
+                    Wil je meer weten over deze functie? Neem dan contact op met Eric Mierop via telefoonnummer
+                    <a
+                        href="tel:+31850027569"
+                    >085-002 7569</a>.
+                </span>
+            </p>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ pages/nieuws/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <b-container style="margin-top: 10rem; margin-bottom: 10rem;">
+        <b-row>
+            <h1>Nieuws</h1>
+        </b-row>
+        <b-row>
+            <b-card
+                no-body
+                class="overflow-hidden mb-3 article"
+                v-on:click="$router.push(`/nieuws/${article.slug}`)"
+                v-for="article in news"
+                :key="article.id"
+            >
+                <b-row no-gutters>
+                    <b-col>
+                        <b-card-body :title="article.title">
+                            <b-card-text>{{article.content}}</b-card-text>
+                        </b-card-body>
+                    </b-col>
+                </b-row>
+            </b-card>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            news: [
+                {
+                    id: 0,
+                    slug: 'vacature-parttime-allround-secretaresse',
+                    title: 'Vacature parttime allround secretaress',
+                    content: 'Wij zoeken een parttime allround secretaresse (v) voor langere periode. In deze uitdagende en veelzijdige functie ben je de rechterhand voor de eigenaar. Je houdt de administratie/boekhouding bij, bent verantwoordelijk voor commerci&euml;le activiteiten en doet alle voorkomende werkzaamheden op kantoor zoals HR en IT. Je bent een echte spin-in-het-web, hebt een pro-actieve houding, bent secuur en kan goed zelfstanding werken. Wij zijn direct en verwachten dit ook van jou.',
+                }
+            ]
+        }
+    }
+};
+</script>
+
+
+<style lang="less" scoped>
+.article {
+    cursor: pointer;
+    transition: 0.2s all;
+
+    &:hover {
+        box-shadow: 0px 5px 6px 1px rgba(0, 0, 0, 0.2);
+    }
+}
+</style>
