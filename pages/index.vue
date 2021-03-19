a7710f4d45e48c1a5e7cec501db649744add29ca
Wes <77240947+Wes-sudo@users.noreply.github.com>
2021-03-19 23:19:36 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
                 <b-row align-h="center">
                     <b-col cols="8">
                         <h1>Wie zijn wij?</h1>
+                        <br>
                         <p>040 Vastgoedontwikkeling is als projectontwikkelaar actief in het ontwikkelen van projecten door geheel Nederland en is gespecialiseerd in de ontwikkeling van vastgoedprojecten in de ouderen- en gehandicaptenzorg. Met onze kennis en ervaring van zowel zorg als projectontwikkeling en een groot netwerk creëren wij huisvestingslocaties voor zorginstellingen. Dit doen wij met een ambitieus team met ruime ervaring en jong talent.</p>
                         <br>
                         <br>
@@ -15,6 +16,7 @@
                 <b-row align-h="center">
                     <b-col cols="8">
                         <h1>Heeft u vragen?</h1>
+                        <br>
                         <p>Heeft u vragen of bent u benieuwd naar de mogelijkheden, dan kunt u contact opnemen met ons via info@040vastgoedontwikkeling.nl</p>
                     </b-col>
                 </b-row>
