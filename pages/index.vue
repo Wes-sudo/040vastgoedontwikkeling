e2bf93885c11ffbc00703061a10a0c3c6d183dfd
Wes <77240947+Wes-sudo@users.noreply.github.com>
2021-03-25 19:49:27 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
                 <b-row align-h="center">
                     <b-col cols="8">
                         <h1>Wie zijn wij?</h1>
-                        <p><br />040 Vastgoedontwikkeling is als projectontwikkelaar actief in het ontwikkelen van projecten door geheel Nederland en is gespecialiseerd in de ontwikkeling van vastgoedprojecten in de ouderen- en gehandicaptenzorg. <br />Met onze kennis en ervaring van zowel zorg als projectontwikkeling en een groot netwerk creëren wij huisvestingslocaties voor zorginstellingen. Dit doen wij met een ambitieus team met ruime ervaring en jong talent.</p>
+                        <p><br />040 Vastgoedontwikkeling is als projectontwikkelaar actief in het ontwikkelen van projecten door geheel Nederland en is gespecialiseerd in de ontwikkeling van vastgoedprojecten in de ouderen- en gehandicaptenzorg. Met onze kennis en ervaring van zowel zorg als projectontwikkeling en een groot netwerk creëren wij huisvestingslocaties voor zorginstellingen. Dit doen wij met een ambitieus team met ruime ervaring en jong talent.</p>
                         <br>
                         <br>
                     </b-col>
